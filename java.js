f86272ae17e1cee1fd14353c7d71a2d622625843
alumno <alumno@Instituto.local>
2025-05-14 08:22:24 -0300

    ejejejej

---
+++ java.js
@@ -0,0 +1,11 @@
+let ArregloColores = ['Amarillo','rojo','azul','verde','marron']
+
+let primero =  ArregloColores [0];
+
+
+console.log (ArregloColores.length)
+console.log(primero)
+
+ArregloColores.push('rosa');
+console.log(ultimo)
+let ultimo = ArregloColores[ArregloColores.length -1];
